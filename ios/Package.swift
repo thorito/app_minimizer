cf80c28e321f1ea8c05840059bde37b64c1be4c6
victor <victor.villar.misa@gmail.com>
2026-06-12 15:54:26 +0200

    fix(ios): correct SPM product name for plugin resolution

--- ios/Package.swift
+++ ios/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "app-minimizer",
+            name: "app_minimizer",
             type: .static,
             targets: ["app_minimizer"]
         )
